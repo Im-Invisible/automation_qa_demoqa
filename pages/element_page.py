a8ad5e0341510320198b696385309bec58a8cbaa
ImInvisible <vitaliysomoniuk@gmail.com>
2022-10-10 21:53:54 +0300

    Added test radio button and update elements test

--- pages/element_page.py
+++ pages/element_page.py
@@ -1,6 +1,6 @@
 import random
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 
 
@@ -60,3 +60,16 @@ class CheckBoxPage(BasePage):
         for item in result_list:
             data.append(item.text)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_on_the_radio_button(self, choice):
+        choices = {'yes': self.locators.YES_RADIO_BUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIO_BUTTON,
+                   'no': self.locators.NO_RADIO_BUTTON}
+        radio = self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
