c7aefe885f439e2ab9753c0aae9c4c69bc72abe0
ImInvisibl <vitaliysomoniuk@gmail.com>
2022-11-09 13:10:49 +0200

    Added item selection test and window resize test

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,8 +1,7 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage, ResizablePage
 
 
 class TestInteractions:
-
     class TestSortablePage:
 
         def test_sortable(self, driver):
@@ -12,3 +11,22 @@ class TestInteractions:
             grid_before, grid_after = sortable_page.change_grid_order()
             assert list_before != list_after, 'The order of the list has not been changed'
             assert grid_before != grid_after, 'The order of the grid has not been changed'
+
+    class TestSelectablePage:
+        def test_resizable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            item_list = selectable_page.select_list_item()
+            item_grid = selectable_page.select_grid_item()
+            assert len(item_list) > 0, 'No elements were selected'
+            assert len(item_grid) > 0, 'No elements were selected'
+
+    class TestResizablePage:
+        def test_resizable(self, driver):
+            resizable_page = ResizablePage(driver, 'https://demoqa.com/resizable')
+            resizable_page.open()
+            max_box, min_box = resizable_page.change_size_resizable_box()
+            max_resize, min_resize = resizable_page.change_size_resizable()
+            assert ('500px', '300px') == max_box, 'Maximum size not equal to "500px", "300px"'
+            assert ('150px', '150px') == min_box, 'Minimum size not equal to "150px", "150px"'
+            assert min_resize != max_resize, 'Resizable has not been changed'
